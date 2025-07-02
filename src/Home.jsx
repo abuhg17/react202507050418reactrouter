c126c353cb1834bb2f84b48a1c2c6cb3559ba1a5
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-02 12:56:48 +0800

    react202507050418reactrouter 0702

--- src/Home.jsx
+++ src/Home.jsx
@@ -102,6 +102,202 @@ export default function HomePage() {
         api youtube channel UCRykoXOF-1jb0DS4P34NG6Q
       </a>
       <hr />
+      <h2>https://daisyui202507050418fresh.deno.dev/</h2>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/hello"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api hello
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/countdown/1111"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api countdown 1111
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/countdown/202507050418"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api countdown 202507050418
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/bilibili/BV1BELHzyEMi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili BV1BELHzyEMi
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/bilibili/BV1Et4y1r7Eu"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili BV1Et4y1r7Eu
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/bilibili/1111"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili 1111
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/bilibili/proxyimg?url=http://i1.hdslb.com/bfs/archive/b168c77ff9d280b3d9f7f8a134ddc9df9c953ec9.jpg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili proxyimg
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/firebasefood"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api firebasefood
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/youtube/videos/WS3sGVgkOZk"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube videos WS3sGVgkOZk
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/youtube/channel/UCTwKpp4T5n1wyxLVf-cD4ew"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube channel UCTwKpp4T5n1wyxLVf-cD4ew
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/youtube/videos/-4ADGW7TE0Q"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube videos -4ADGW7TE0Q
+      </a>
+      <hr />
+      <a
+        href="https://daisyui202507050418fresh.deno.dev/api/youtube/channel/UCRykoXOF-1jb0DS4P34NG6Q"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube channel UCRykoXOF-1jb0DS4P34NG6Q
+      </a>
+      <hr />
+      <h2>https://elysia202507050418.onrender.com/</h2>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/hello"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api hello
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/countdown/1111"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api countdown 1111
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/countdown/202507050418"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api countdown 202507050418
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/bilibili/BV1BELHzyEMi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili BV1BELHzyEMi
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/bilibili/BV1Et4y1r7Eu"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili BV1Et4y1r7Eu
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/bilibili/1111"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili 1111
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/bilibili/proxyimg?url=http://i1.hdslb.com/bfs/archive/b168c77ff9d280b3d9f7f8a134ddc9df9c953ec9.jpg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api bilibili proxyimg
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/firebasefood"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api firebasefood
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/youtube/videos/WS3sGVgkOZk"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube videos WS3sGVgkOZk
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/youtube/channel/UCTwKpp4T5n1wyxLVf-cD4ew"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube channel UCTwKpp4T5n1wyxLVf-cD4ew
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/youtube/videos/-4ADGW7TE0Q"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube videos -4ADGW7TE0Q
+      </a>
+      <hr />
+      <a
+        href="https://elysia202507050418.onrender.com/api/youtube/channel/UCRykoXOF-1jb0DS4P34NG6Q"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        api youtube channel UCRykoXOF-1jb0DS4P34NG6Q
+      </a>
+      <hr />
     </div>
   );
 }
